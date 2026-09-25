b0ae267954cee8fd2d6a9eeda99e1dc873e1c3c0
Olesia Z. <zin00les@gmail.com>
2026-09-25 12:33:18 -0700

    Set mirror base path for routing

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Corp_OS">
       <UserModeProvider>
         <TourHighlightProvider>
           <PlasmaTheme />
